8feaed4c20ef358572630e80b6d5a3286f2e6e6e
Harry Long <harrydhlong@gmail.com>
2020-09-18 15:42:55 +0100

    Updated code

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,7 +3,22 @@
 require_relative 'bike'
 
 class DockingStation
-  def release_bike
-    Bike.new
+  def initialize
+    @bikes = []
   end
+
+  def release_bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
+  end
+
+  def dock(bike)
+    # Use an instance variable to store the bike
+    # in the 'state' of this instance
+    fail 'Docking station full' if @bikes.count >= 20
+    @bikes << bike
+  end
+
+  # Let's add to our #bike method to return the bike we docked
+  attr_reader :bike
 end
