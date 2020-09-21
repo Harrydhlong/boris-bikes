7435a73a9b3101855e61c7574a64773db55878d7
Harry Long <harrydhlong@gmail.com>
2020-09-21 15:11:18 +0100

    update

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,22 +3,35 @@
 require_relative 'bike'
 
 class DockingStation
-  def initialize
+  DEFAULT_CAPACITY = 20
+
+  attr_reader :capacity
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @capacity = capacity
     @bikes = []
   end
 
+
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    # Use an instance variable to store the bike
-    # in the 'state' of this instance
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if full?
     @bikes << bike
   end
 
-  # Let's add to our #bike method to return the bike we docked
-  attr_reader :bike
+  private
+
+  attr_reader :bikes
+
+  def full?
+    @bikes.count >= capacity
+  end
+
+  def empty?
+    @bikes.empty?
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,19 +18,41 @@ describe DockingStation do
       # remember subject == DockingStation.new
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
+    it 'raises an error when there is a broken bike' do
+      bike = Bike.new
+      bike.report_broken
+      subject.dock bike
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
   describe '#dock' do
     it 'raises an error when full' do
-      20.times { subject.dock Bike.new }
+      subject.capacity.times do
+        subject.dock Bike.new
+      end
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
 
+  describe 'initialization' do
+    subject { DockingStation.new }
+    let(:bike) { Bike.new }
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.dock(bike)
+      end
+      expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+    end
+  end
+
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
 
   it { is_expected.to respond_to :release_bike }
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it { is_expected.to respond_to(:bike) }
+
 end
