8a1a71616458f04b0b0f88b7c2a0d9f5a99dae1c
Harry Long <harrydhlong@gmail.com>
2020-09-22 23:55:16 +0100

    update

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,17 +1,16 @@
 require 'docking_station'
 
 describe DockingStation do
-
+  it { is_expected.to respond_to :release_bike }
   # Let's upgrade our syntax, to:
   # use a '#' before a method name
   # to imply that it is an instance
   # method. Also look: nested describes!
   describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      # We want to release the bike we docked
-      expect(subject.release_bike).to eq bike
+    it 'releases working bikes' do
+      bike = double(:bike, broken?: false)
+      subject.dock bike
+      expect(subject.release_bike).to be bike
     end
     it 'raises an error when there are no bikes available' do
       # Let's not dock a bike first:
@@ -19,8 +18,7 @@ describe DockingStation do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
     it 'raises an error when there is a broken bike' do
-      bike = Bike.new
-      bike.report_broken
+      bike = double(:bike, broken?: true)
       subject.dock bike
       expect { subject.release_bike }.to raise_error 'Bike is broken'
     end
@@ -29,9 +27,9 @@ describe DockingStation do
   describe '#dock' do
     it 'raises an error when full' do
       subject.capacity.times do
-        subject.dock Bike.new
+        subject.dock double :bike
       end
-      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+      expect { subject.dock double(:bike) }.to raise_error 'Docking station full'
     end
   end
 
@@ -49,8 +47,6 @@ describe DockingStation do
   it 'has a default capacity' do
     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
-
-  it { is_expected.to respond_to :release_bike }
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
